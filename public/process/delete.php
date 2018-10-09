eec05393891419f3de19cfeb47a90cfe6930ba09
ivelin <ivelin952@gmail.com>
2018-10-08 22:39:50 -0700

    delete.php - Invoice deletion logic

---
+++ public/process/delete.php
@@ -0,0 +1,31 @@
+<?php
+
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+
+    if ($_GET['id'] > 0) {
+
+        require_once "../../config/database.php";
+
+        $invoiceId = $_GET['id'];
+
+        $sql = 'DELETE FROM invoices WHERE id = ?';
+        $statement = $dbCon->prepare($sql);
+        $statement->bind_param('i', $invoiceId);
+        $statement->execute();
+
+        $sql = 'DELETE FROM invoice_products WHERE invoice_id = ?';
+        $statement = $dbCon->prepare($sql);
+        $statement->bind_param('i', $invoiceId);
+        $statement->execute();
+
+        $sql = 'DELETE FROM invoice_payments WHERE invoice_id = ?';
+        $statement = $dbCon->prepare($sql);
+        $statement->bind_param('i', $invoiceId);
+        $statement->execute();
+    }
+
+    header('Location: ' . '../index.php');
+    die();
+}
+
+
